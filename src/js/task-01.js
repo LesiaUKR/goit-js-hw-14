0fcfb7e8bb6313b869d6dc17a72255801b58529f
Your Name <solovyova.lesya@gmail.com>
2023-10-19 23:34:39 +0300

    task 1-5 solved

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,3 +1,35 @@
-const quantityOfCategories = () => {};
+/*
+Порахує і виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
+Для кожного элемента li.item у списку ul#categories, знайде і виведе в консоль текст заголовку елемента (тегу <h2>) і кількість елементів в категорії (усіх <li>, вкладених в нього).
+В результаті, в консолі будуть виведені наступні повідомлення.
 
-console.log();
+Number of categories: 3
+
+Category: Animals
+Elements: 4
+
+Category: Products
+Elements: 3
+
+Category: Technologies
+Elements: 5
+*/
+
+const categories = document.querySelectorAll("li.item");
+const quantityOfCategories = categories.length;
+
+console.log("Number of categories:", quantityOfCategories);
+
+categories.forEach((element) => {
+  console.log("Category:", element.firstElementChild.textContent);
+  console.log("Elements:", element.lastElementChild.childElementCount);
+});
+
+/********** 2nd variant **********/
+console.log("/****************************/");
+
+for (let i = 0; i <= categories.length; i++) {
+  const category = categories[i];
+  console.log("Category:", category.firstElementChild.textContent);
+  console.log("Elements:", category.lastElementChild.childElementCount);
+}
--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,47 @@
+/*
+<ul id="ingredients"></ul>
+
+Напиши скрипт, який для кожного елемента масиву ingredients:
+
+Створить окремий елемент <li>. Обов'язково використовуй метод document.createElement().
+Додасть назву інгредієнта як його текстовий вміст.
+Додасть елементу клас item.
+Після чого, вставить усі <> за одну операцію у список ul#ingredients.
+*/
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const list = document.createElement("ul");
+list.setAttribute("id", "ingredients");
+
+const listItemMarkup = () => {
+  for (let i = 0; i < ingredients.length; i++) {
+    const listItem = document.createElement("li");
+    listItem.classList.add("item");
+    listItem.textContent = ingredients[i];
+    // console.log(listItem);
+    list.appendChild(listItem);
+  }
+};
+
+listItemMarkup();
+
+document.body.appendChild(list);
+
+/********** 2nd variant **********/
+
+// const listMarkup = ingredients.map((ingredient) => {
+//   const listItem = document.createElement("li");
+//   listItem.classList.add("item");
+//   listItem.textContent = ingredient;
+//   // console.dir(listItem);
+//   // console.log(listItem);
+
+//   list.appendChild(listItem);
+// });
